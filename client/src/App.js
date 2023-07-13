ae573c9433741814f58ceb280888b83e694e2030
shahryar jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-07-13 22:02:35 +0330

    fixed api calls urls

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+axios.defaults.baseURL = 'http://server-cluster-ip-service:3001/'
+
+function App() {
+  const [seenIndexes, setSeenIndexes] = useState([])
+  const [values, setValues] = useState({})
+  const [index, setIndex] = useState('')
+
+  useEffect(() => {
+    const fetch = async () => {
+      const values = await axios.get('/values/current')
+      const seenIndexes = await axios.get('/values/all')
+      setValues(values.data)
+      setSeenIndexes(seenIndexes.data)
+    }
+    fetch()
+  }, [])
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    await axios.post('/values', {
+      index: index,
+    })
+    setIndex('')
   }
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1 className='App-title'>Fib Calculator version 2</h1>
+      </header>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={index}
+            onChange={event => setIndex(event.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+
+        <h3>Indexes I have seen:</h3>
+        {seenIndexes.map(({ number }) => number).join(', ')}
+
+        <h3>Calculated Values:</h3>
+        {values.map((index, value) => {
+          return (
+            <div key={index}>
+              For index {index} I calculated {value}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- server/index.js
+++ server/index.js
@@ -1,10 +1,54 @@
 import express from 'express'
+import cors from 'cors'
+import { Pool } from 'pg'
+import { createClient } from 'redis'
 
 const app = express()
-
 const PORT = process.env.PORT | 3001
-
+app.use(cors())
 app.use(express.json())
+
+const pgClient = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
+
+pgClient.on('connect', () => {
+  pgClient
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.log(err))
+})
+
+const redisClient = createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  retry_strategy: () => 1000,
+})
+const redisPublisher = redisClient.duplicate()
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * from values')
+  res.send(values.rows)
+})
+
+app.get('/values/current', async (req, res) => {
+  const values = await redisClient.hGetAll('values')
+  res.send(values)
+})
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index
+  if (+index > 40) {
+    return res.status(422).send('Index too high')
+  }
+  redisClient.hSet('values', index, 'Nothing yet!')
+  redisPublisher.publish('insert', index)
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index])
+  res.send({ working: true })
+})
 
 app.get('*', (req, res) => {
   res.status(200).send('successful GET request!')
